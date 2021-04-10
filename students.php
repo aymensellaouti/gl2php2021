8b5b5a73b6ad3ff4f8a8f581b0b04c23386d11de
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-10 10:15:35 +0100

    exemples cours php

---
+++ students.php
@@ -0,0 +1,10 @@
+<?php
+$students = [
+    'fatma' => 'laribi',
+    'ahmed' => 'mahfoudhi',
+    'salem' => 'hamdani'
+];
+
+foreach($students as $firstname => $name) {
+    echo "Bienvenu $firstname $name <br>";
+}

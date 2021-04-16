af754ecd4477e4970d0967a17f2f512fb6c0668a
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-16 09:01:16 +0100

    session + form

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'isAuthenticated.php';
+$pageTitle = 'home';
+include_once 'fragments/header.php';
+?>
+
+<div class="alert alert-success">
+    Bienvenu chez vous
+</div>
+
+</body>
+</html>
